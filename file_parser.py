7d88b64e0aaa0e59e8386a7d401f563d4c5220d5
swapniliitk <32162781+swapniliitk@users.noreply.github.com>
2019-08-22 17:00:15 +0200

    Add files via upload
    Updates: File parser function in file_parser.py is updated!

--- file_parser.py
+++ file_parser.py
@@ -30,40 +30,54 @@ class File_Parser(object):
                 continue
             elif ((data[0]=="[") and (data[1]=="atomtypes") and (data[2]=="]")):
                 flag=1
+                sign="atomtypes"
                 continue
             elif ((data[0]=="[") and (data[1]=="atoms") and (data[2]=="]")):
                 flag=1
+                sign="atoms"
                 continue
             elif ((data[0]=="[") and (data[2]=="]")):
                 flag = 0
             if ((flag==1) and data[0] != ";" and len(data)):
                 i=i+1
-                for temp in data:
-                    print (temp)
-        print ("i=" + str(i))
+                File_Parser.create_vectors(sign,data)
         f.close()
         
     def parse_txt(path):
         f = open (path, "r")
         print (path)
+        i=0
+        
         for x in f.readlines():
             data=x.split()
-            if (len(data) < 3):
+            if ((data[0]=="fitness") or (data[0]=="final")):
                 continue
-            elif ((data[0]=="[") and (data[1]=="atomtypes") and (data[2]=="]")):
-                flag=1
-                continue
-            elif ((data[0]=="[") and (data[1]=="atoms") and (data[2]=="]")):
-                flag=1
-                continue
-            elif ((data[0]=="[") and (data[2]=="]")):
-                flag = 0
-            if ((flag==1) and data[0] != ";" and len(data)):
-                i=i+1
-                for temp in data:
-                    print (temp)
+            else:
+                sign = "out"
+                File_Parser.create_vectors(sign, data)
         print ("i=" + str(i))
         f.close()        
         
-#        def create_dictionary(case, info):         Search for "Python Dictionary" and work on this method to fetch the data and create input and output vectors
-            
+    def create_vectors(case, info):
+        i_vector = []
+        o_vector = []
+        if (case == "atoms" or case == "atomtypes"):
+            if (case == "atoms"):
+                for i in len(i_vector):
+                    if info[1] == items:
+                        info[6]=i_vector[1]
+                    else:
+                        i_vector[:][0].append(info[1])
+                        i_vector[:][1].append(info[6])
+            if (case == "atomtypes"):
+                for i in len(i_vector):
+                    if info[0] == items:
+                        i_vector[:][2].append(info[5])
+                    else:
+                        i_vector[:][0].append(info[0])
+                        i_vector[:][2].append(info[5])
+            elif (case == "atomtypes"):
+                i_vector [:][1] = info [5]
+        else:
+            print (case, len(info))
+            print (info)
