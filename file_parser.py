aee67e28b9b76a9d818277983d2f49f508f4771f
swapniliitk <32162781+swapniliitk@users.noreply.github.com>
2019-09-01 18:05:10 +0200

    Add files via upload

--- file_parser.py
+++ file_parser.py
@@ -3,62 +3,52 @@ numpy.set_printoptions(suppress=True)
 
 class File_Parser(object):    
 
-    global o_vector
-    global i_vector
-    global atomnames
-    o_vector = numpy.empty((82,2))
-    i_vector = numpy.empty(82)
-    atomnames = ['' for atomnames in range(82)]
+#    global o_vector
+#    global i_vector
+#    global atomnames
+#    o_vector = numpy.empty((82,2))
+#    i_vector = numpy.empty(82)
+#    atomnames = ['' for atomnames in range(82)]
+    
+    def __init__(self, i_path, o_path, atomtypes, optypes):
+        self.i_path = i_path
+        self.o_path = o_path
+        self.atomtypes = atomtypes
+        self.optypes = optypes
     
     
-    def __init__(self, path, id):
-        self.id = id
-        self.path= path
-        
-    def txt_parser(self, path):
-        self.parse_txt(path)
-
-    def itp_parser(self):
-        self.parse_itp(self.path)
-        
-    def parse_txt(self, path):
-        f = open (path, "r")
-        print (path)
-        for x in f.readlines():
-            data=x.split()
-            if (len(data)==5):
-                for temp in data:
-                    print (temp)
-        f.close()
+    def file_parser(self, i_path, o_path, atomtypes, optypes):
+        i_list = (self.parse_txt(i_path))
+        o_list = (self.parse_itp(o_path))
+        return(self.create_vectors(i_list, o_list, atomtypes, optypes))
     
     def parse_itp(self, path):
         f = open (path, "r")
         flag = 0
         content = []
-        print (path)
         for x in f.readlines():
             data=x.split()
             if (len(data) < 3):
                 continue
             elif ((data[0]=="[") and (data[1]=="atomtypes") and (data[2]=="]")):
-                flag=1
-                sign="atomtypes"
+                flag = 1
+                sign = "atomtype"
                 continue
             elif ((data[0]=="[") and (data[1]=="atoms") and (data[2]=="]")):
-                flag=1
-                sign="atoms"
+                flag = 1
+                sign = "atom"
                 continue
             elif ((data[0]=="[") and (data[2]=="]")):
                 flag = 0
+                
             if ((flag==1) and data[0] != ";" and len(data)):
                 x = x + sign
                 content.append(x)
-        self.create_vectors(content)
         f.close()
+        return(content)
         
     def parse_txt(self, path):
         f = open (path, "r")
-        print (path)
         i=0
         content=[]
         for x in f.readlines():
@@ -66,36 +56,37 @@ class File_Parser(object):
             if ((data[0]=="fitness") or (data[0]=="final")):
                 continue
             else:
-                sign = "out"
-                x = x + sign
                 content.append(x)
-        self.create_vectors(content)
-        print ("i=" + str(i))
         f.close()        
+        return(content)
         
-    def create_vectors(self, info):
-        j=0
-        for line in info:
+    def create_vectors(self, i_list, o_list, atomtypes, optypes):
+        i_vector = numpy.empty(len(self.optypes))
+        o_vector = numpy.empty((len(self.atomtypes)*2))
+        for line in i_list:
             y = line.split()
-            if (y[-1] == "atoms" or y[-1] == "atomtypes"):
-                if (y[-1] == "atomtypes"):
-                    if (y[0] in atomnames):
-                        print ("double entry" + y[0])
-                    else:
-                        atomnames[j] = y[0]
-                        o_vector[j][0] = round(float(y[5]),6)
-                        j = j+1
-                elif (y[-1] == "atoms"):
-                    if (y[1] in atomnames):
-                        index = atomnames.index(y[1])
-                        o_vector[index][1] = round(float(y[6]),3)
-                    else:
-                        print ("atomname not found in 'atoms' section")
-                        print (y[1])
-            elif (y[-1] == "out"):
-                i_vector[j] = round(float(y[3]),3)
-        print (atomnames)
-        print (i_vector)
-        print (o_vector)
+            if y[0] in self.optypes:
+                i_vector[self.optypes.index(y[0])] = round(float(y[2]), 10)
+            else: 
+                i_vector[self.optypes.index(y[0])] = numpy.NaN
+                print(y[0] + "does not exist in the list of optypes")
+
+        for line in o_list:
+            y = line.split()
+            if (y[-1] == "atomtype"):
+                if y[0] in self.atomtypes:
+                    o_vector[self.atomtypes.index(y[0])] = round(float(y[5]),10)
+                else:
+                    print(y[0] + "does not exist in the list of atomtypes: ATOMTYPE secion")
+            elif (y[-1] == "atom"):
+                if y[1] in self.atomtypes:
+                    o_vector[self.atomtypes.index(y[1]) + len(self.atomtypes)] = round(float(y[6]),5)
+                else:
+                    print(y[1] + "does not exist in the list of atomtypes: ATOM secion")
+            else:
+                print("Check the entry " + line)
+                o_vector[self.atomtypes.index(y[0])] = numpy.NaN
+                o_vector[self.atomtypes.index(y[0]) + len(self.atomtypes)] = numpy.NaN
+        return (i_vector, o_vector)
         
     
--- reader.py
+++ reader.py
@@ -1,26 +1,77 @@
 #! /usr/env/python
 
 import os
+import numpy
+from os import path
 from file_parser import File_Parser
 
-i=0 
-path= '/Users/wagle/Downloads/FF_for_swapnil/'
+
+path= '/Users/wagle/Downloads/FF_for_swapnil/test/head_n_tails2'
 files =[]
+atomtypes = []
+optypes = []
+global i_vectors
+global o_vectors
+
+
+f = open('./atomtypes.txt', "r")
+i=0
+for x in f.readlines():
+    data = x.split()
+    atomtypes.append(data[0])
+    i = i+1
+f.close()
+
+i=0
+f = open('./optypes.txt', "r")
+for x in f.readlines():
+    data = x.split()
+    optypes.append(data[0])
+    i = i+1
+f.close()
+
+i_vectors = numpy.empty([0, len(optypes)], dtype=numpy.float64)
+o_vectors = numpy.empty([0, len(atomtypes)*2], dtype=numpy.float64)
+
+print(i_vectors.size, o_vectors.size)
+
+
+#i_vectors = numpy.reshape(i_vectors,(len(optypes),1))
+#o_vectors = numpy.reshape(o_vectors,(len(atomtypes),1))
+
+i=0
 for r, d, f in os.walk(path):
     for file in f:
-        files.append(os.path.join(r,file))
-
-for f in files:
-        instance= File_Parser(f, i)
-        if f.endswith('.txt'):
-            instance.txt_parser(f)
-            break
-        elif f.endswith('.itp'):
+        if file.endswith(".txt"):
+            files.append(os.path.join(r, file))
+    for fff in sorted(files):
+        if ((os.path.exists(fff)) and (os.path.getsize(fff) == 0)):
+            print ("Warning type 1: File exists but is empty " , fff)
             continue
-            instance.itp_parser()
-            break
+        elif (not (os.path.exists(fff))):
+            print ("Warning type 2: txt file does not exists" , fff)
+            continue
         else:
-            print (f)
+            txt_filepath = fff
+            
+        itp_filename = "lipid_" + os.path.splitext(fff)[0].split('_')[-2] + "_" + os.path.splitext(fff)[0].split('_')[-1] + ".itp"
+        itp_filepath = os.path.join(os.path.dirname(fff), itp_filename)
+        if (os.path.exists(itp_filepath) and (os.path.getsize(itp_filepath)) == 0):
+            print ("Warning type 1: File exists but is empty " , itp_filepath)
             continue
-        i=i+1
+        elif (not (os.path.exists(itp_filepath))):
+            print ("Warning type 2: itp file does not exists" , itp_filepath)
+            continue
+        else:
+            print (txt_filepath, itp_filepath)
+            i_vector = numpy.array([len(optypes)], dtype=numpy.float64)
+            o_vector = numpy.array([len(atomtypes)*2], dtype=numpy.float64)
+            instance = File_Parser(txt_filepath, itp_filepath, atomtypes, optypes)
+            (i_vector, o_vector) = zip(instance.file_parser(txt_filepath, itp_filepath, atomtypes, optypes))
+            i_vectors = numpy.append(i_vectors, i_vector, axis =0)
+            o_vectors = numpy.append(o_vectors, o_vector, axis =0)
+            print(i_vectors, o_vectors)
+            print(len(i_vectors), len(o_vectors))
+            i= i+1
+            
         
